0707040a6fa7cd37cb83124b0a0730f9a018e77e
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2025-12-10 19:49:37 +0200

    Added unit tests

---
+++ src/test/java/com/file/registry/constants/TestApplicationConstants.java
@@ -0,0 +1,10 @@
+package com.file.registry.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class TestApplicationConstants {
+
+    public static final String JSON_FILE_NAME = "testcustomer_docs_2025-12-16.json";
+    public static final String XML_FILE_NAME = "testcustomer_docs_2025-12-16.xml";
+}
